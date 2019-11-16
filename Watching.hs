f849743a24868db7c5f665a2f9d07c6563e525bd
Jediah Katz <jediahkatz@gmail.com>
2019-11-16 15:40:20 -0500

    Changed watchedlits types

--- Watching.hs
+++ Watching.hs
@@ -9,22 +9,28 @@ import Data.List (find)
 import Formula
 
 type Watchlist = IntMap [Clause]
-type WatchedLits = Map Clause (Lit, Lit)
+
+type WatchedLits = Either Lit (Lit, Lit)
+  
+type WatchedLitsMap = Map Clause WatchedLits
 
 -- Get a list of clauses watching a literal.
 clausesWatching :: Lit -> Watchlist -> [Clause]
 clausesWatching = findWithDefault []
 
 -- Is this clause watching this literal?
-isWatching :: Clause -> Lit -> WatchedLits -> Bool
+isWatching :: Clause -> Lit -> WatchedLitsMap -> Bool
 isWatching c l w = case (Map.lookup c w) of
-  Nothing       -> False
-  Just (l1, l2) -> l == l1 || l == l2
+  Nothing               -> False
+  Just (Left l')        -> l == l'
+  Just (Right (l1, l2)) -> l == l1 || l == l2
 
 -- Attempt to find a non-False literal in the
 -- clause which is not already being watched.
 -- Does the WatchedLits lookup only happen once due to laziness?
-findNewWatchedLit :: Clause -> WatchedLits -> Assignment -> Maybe Lit
+findNewWatchedLit :: Clause -> WatchedLitsMap -> Assignment -> Maybe Lit
 findNewWatchedLit c w a = find (\l -> not (isWatching c l w || val l a == F)) c
+
+
 
 
