2b7372cd1b57df2adea45dd9f520c335bb04cd6f
Jediah Katz <jediahkatz@gmail.com>
2019-11-18 15:19:26 -0500

    Restructuring

--- Formula.hs
+++ Formula.hs
@@ -1,11 +1,6 @@
 module Formula where
 
---  Eventually test with Strict variants
-import Data.IntMap.Lazy (IntMap, findWithDefault, insert)
---import qualified Data.IntMap.Lazy as IntMap
-import Control.Monad.State.Lazy(State, get, put)
---import State (State)
-import Data.List (find)
+import Data.IntMap.Lazy (IntMap, findWithDefault)
 
 type Var = Int
 type Lit = Int
@@ -36,61 +31,3 @@ data Value = T | F | U
 -- Get the value of a literal in a given assignment.
 val :: Lit -> Assignment -> Value
 val = findWithDefault U
-
--- Solver state containing number of variables, current assignment
--- and propagation queue (data structures can change).
-data SolverState = SS {
-  n :: Int,
-  ass :: Assignment,
-  propQ :: [Lit]
-}
-
--- Attempt to assign given literal to True.
--- If it was already False, return False;
--- Otherwise return True and add its negation
--- to the end of the propQ if it was unassigned.
-assume :: Lit -> State SolverState Bool
-assume x = do
-  ss@(SS {ass=a, propQ=q}) <- get
-  case (val x a) of
-    F -> return False
-    T -> return True
-    U -> (do
-      put (ss {ass=insert x T a, propQ = (neg x):q})
-      return True)
-
--- Repeatedly attempt unit propagation until the
--- propagation queue is empty, updating the SolverState.
--- If at any point, we encounter a conflict, then
--- unitPropagate returns False. Otherwise it returns True.
-unitPropagate :: State SolverState Bool
-unitPropagate = undefined
-
--- Return the next decision literal, or Nothing
--- if all literals have been assigned.
-pickLiteral :: State SolverState (Maybe Lit)
-pickLiteral = do
-  SS {ass=a, n=n'} <- get
-  return (find (\l -> val l a == U) [1..n'])
-
--- Stateful DPLL helper that attempts to find a satisfying
--- assignment given the partial assignment represented by
--- its initial state. It returns True if one is found, and 
--- False if we encounter a conflict.
-dpllHelper :: State SolverState Bool
-dpllHelper = undefined
---dpllHelper = do
---  b <- unitPropagate
---  // return False if b is False
---  x <- pickLiteral
---  // return True if x is Nothing
---  ss <- get
---  assume x
---  b' <- dpllHelper
---  // return True if b is True
---  put ss
---  assume (not x)
---  b'' <- dpllHelper
---  return b''
-  
-solve :: CNF -> Maybe Assignment
