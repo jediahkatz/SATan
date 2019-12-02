ad55b7c2eee655a0d593007d3d15b178d2150c14
josh <joshcohen98@gmail.com>
2019-12-02 13:18:28 -0500

    added parsing for dimacs

--- Main.hs
+++ Main.hs
@@ -1,23 +1,38 @@
 module Main where
 
+import Formula(CNF, Clause)
 import Solver(solve)
 import Data.IntMap.Lazy (toList)
 import Text.Read(readMaybe)
 
 main :: IO ()
 main = do
-  putStrLn "Welcome! Press s to solve a formula or e to exit."
+  putStrLn "Welcome! Enter a filename or enter e to exit."
   str <- getLine
   case str of
     "e" -> return ()
-    "s" -> do
-        putStrLn "Please enter a CNF in the form of a list of lists of nonzero numbers."
-        putStrLn "For instance, the list '[[1, 2], [-1]]' represents the formula (x_1 \\/ x_2) /\\ not(x_1)"
-        s <- getLine
-        case (readMaybe s) of
-          Nothing -> putStrLn "Sorry, I didn't understand that. Make sure you format your formula as a list. Please try again" >> main
-          Just r ->
-            putStrLn (case (solve r) of
-                        Nothing -> "Unsatisfiable" 
-                        Just x -> "Satisfiable: A satisfying assignment is " ++ show (toList x)) >> main
-    _ -> putStrLn "Sorry, I didn't understand that. Please try again." >> main
+    _ -> do
+     contents <- readFile str
+     case parseDimacs contents of
+      Nothing -> putStrLn "Sorry, I didn't understand that. Please try again." >> main
+      Just cnf -> putStrLn (case solve cnf of
+                              Nothing -> "Unsatisfiable"
+                              Just x -> "Satisfiable: A satsifying assignment is " ++ show (toList x)) >> main
+
+parseDimacs :: String -> Maybe CNF
+parseDimacs s = foldr (\x acc -> case ((dimacsLine x), acc) of
+                                  (Nothing, _) -> acc
+                                  (Just y, Just z) -> Just (y : z)
+                                  (Just y, _) -> Just [y]) Nothing (lines s)
+
+dimacsLine :: String -> Maybe Clause
+dimacsLine ('c' : _) = Nothing
+dimacsLine ('p' : _) = Nothing
+dimacsLine l = foldr (\x acc -> case (x, acc) of
+                                  ("0", Nothing) -> acc
+                                  (c, Nothing) -> case readMaybe c of
+                                                    Nothing -> Nothing
+                                                    Just z -> Just [z]
+                                  (c, Just y) ->  case readMaybe c of
+                                                    Nothing -> Nothing
+                                                    Just z -> Just (z : y)) Nothing (words l) 
