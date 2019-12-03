3c59f57ea7a718d74ac3b7725bfe691a33a59271
josh <joshcohen98@gmail.com>
2019-12-03 17:14:04 -0500

    added back interaction for IO

--- Main.hs
+++ Main.hs
@@ -7,17 +7,31 @@ import Text.Read(readMaybe)
 
 main :: IO ()
 main = do
-  putStrLn "Welcome! Enter a filename or enter e to exit."
-  str <- getLine
-  case str of
+  putStrLn "Welcome! Enter f to solve from a file, i for interactive, or enter e to exit."
+  c <- getLine
+  case c of
     "e" -> return ()
-    _ -> do
-     contents <- readFile str
-     case parseDimacs contents of
-      Nothing -> putStrLn "Sorry, I didn't understand that. Please try again." >> main
-      Just cnf -> putStrLn (case solve cnf of
-                              Nothing -> "Unsatisfiable"
-                              Just x -> "Satisfiable: A satsifying assignment is " ++ show (toList x)) >> main
+    "f" -> do
+       putStrLn "Please enter a filpath with a SAT formula in DIMACS format."
+       str <- getLine
+       contents <- readFile str
+       case parseDimacs contents of
+        Nothing -> putStrLn "Sorry, I didn't understand that. Please try again." >> main
+        Just cnf -> putStrLn (case solve cnf of
+                                Nothing -> "Unsatisfiable"
+                                Just x -> "Satisfiable: A satsifying assignment is " ++ show (toList x)) >> main
+    "i" -> do
+        putStrLn "Please enter a CNF in the form of a list of lists of nonzero numbers."
+        putStrLn "For instance, the list '[[1, 2], [-1]]' represents the formula (x_1 \\/ x_2) /\\ not(x_1)"
+        s <- getLine
+        case (readMaybe s) of
+          Nothing -> putStrLn "Sorry, I didn't understand that. Make sure you format your formula as a list. Please try again" >> main
+          Just r ->
+            putStrLn (case (solve r) of
+                        Nothing -> "Unsatisfiable" 
+                        Just x -> "Satisfiable: A satisfying assignment is " ++ show (toList x)) >> main
+    _ -> putStrLn "Sorry, I didn't understand that. Please try again." >> main 
+
 
 parseDimacs :: String -> Maybe CNF
 parseDimacs s = foldr (\x acc -> case ((dimacsLine x), acc) of
